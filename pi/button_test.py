203058be7e9f625b5b67f94ce105c5aff2094d9a
michael <mishacottow@gmail.com>
2024-03-20 21:10:08 -0400

    buttons

--- pi/button_test.py
+++ pi/button_test.py
@@ -10,11 +10,11 @@ FN4_PIN = 21
 FN5_PIN = 26
 SEL_PIN = 19
 
-class btn: 
+class Btn: 
     def __init__(self, pin, name):
         self.name = name
         self.pin = pin
-        logging.debug(f"Created button with pin={self.pin} name={self.name}")
+        logging.info(f"Created button with pin={self.pin} name={self.name}")
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=self.onchange, bouncetime=10)
 
@@ -31,12 +31,12 @@ def signal_handler(sig, frame):
 
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BCM)
-    btn(FN1_PIN, "Func1")
-    btn(FN2_PIN, "Func2")
-    btn(FN3_PIN, "Func3")
-    btn(FN4_PIN, "Func4")
-    btn(FN5_PIN, "Func5")
-    btn(SEL_PIN, "Selector")
+    fn1 = Btn(FN1_PIN, "Func1")
+    fn2 = Btn(FN2_PIN, "Func2")
+    fn3 = Btn(FN3_PIN, "Func3")
+    fn4 = Btn(FN4_PIN, "Func4")
+    fn5 = Btn(FN5_PIN, "Func5")
+    sel = Btn(SEL_PIN, "Selector")
 
     signal.signal(signal.SIGINT, signal_handler)
     logging.info("Press CTRL-C to exit.")
